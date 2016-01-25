8cebff085c3be82ded200e8043ced1efcc991df5
eljun <el.junsan@yahoo.com>
2016-01-25 17:24:27 +0800

    Content Updates

---
+++ assets/js/custom.js
@@ -0,0 +1,23 @@
+// $('.email').change(function(){
+//    $('.cc').val($(this).val());
+// })
+
+// $('form').submit(function(e){
+
+// e.preventDefault();
+
+// var action = $(this).attr('action');
+
+//    $.ajax({
+//      url: action,
+//      type: "POST",
+//      data: $('form').serialize(),
+//      dataType: "json",
+//      success: function(data){
+//        console.log(data)
+//      },
+//      error: function(error){
+//        alert("Please Type again")
+//      }
+//    })
+// });
