906acdb52a3ca8b729fff20dd78ae136e73ea6a3
allen_support <allen.support@webfactories.biz>
2016-02-02 18:13:42 +0800

    adding validation and captcha, fixing logo alignment from Jeb

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -49,3 +49,59 @@ $('li.dropdown > .dropdown-menu li').each(function(){
         $(this).parents('li.dropdown').find("a.dropdown-toggle").addClass("active");
     } 
 });
+
+ var debug = true;  
+  $(document).on('submit', 'form#ss-form', function(e) {
+      e.preventDefault();
+    var form = $(this);
+    if ( debug){
+      console.log($(this).serialize());
+    }
+        $.ajax({
+          url: $(this).attr('action'),
+          type: "POST",
+          data: $(this).serialize(),
+          async : false,
+          
+        }).always(function(){
+          
+          form.find('input[type=text]').val('');
+
+      window.top.location = window.top.location;
+      alert('Thanks! Your response has been recorded.');
+      
+      
+        });
+
+    });
+
+var formID = 'ss-form';
+var labelName = 'ssTestLabel';
+var testField = 'ssTestValue';
+var submitted = false;
+
+
+ var ssForm = $('#'+formID);
+ var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+ var string_length = 5;
+ var randomstring = '';
+
+for (var i=0; i<string_length; i++) {
+ var randomInt = Math.floor(Math.random()*chars.length);
+ randomstring += chars.substring(randomInt,randomInt+1);
+  
+}
+  
+ $('#'+labelName).html('<p>If you\'re human, type "<strong>'+ randomstring +'</strong>" in the box given below</p>');
+  
+ ssForm.submit(function(evt){
+  
+if($('#'+testField).val() == randomstring){
+   ssForm.attr({'action' : '//formspree.io/ayenzky101@gmail.com'});
+   return true;
+   }
+else{
+   alert('You need to repeat the number "' + randomstring + '"');
+    return false;
+  }
+ })
